aef61cb5c06ae33e8d945a19b32ac82208dc7389
gerald923 <gsupom@unsa.edu.pe>
2024-11-13 03:08:41 +0000

    en el commit anterior el problema que teniamos era la varieble que estaba en stack en allocateMemory y lo arreglamos usando triple puntero que al parecer accede al newfig pero de la funcion reverse y desde ahi podemos hacer los cambios, tambien teniamos que modificar el unlinkmemory para que al liberar memoria tambien afecte a newfig, a pesar de eso salio un error mesa glx pero se corrigio el error

--- chess.c
+++ chess.c
@@ -3,18 +3,18 @@
 #include "chess.h"
 #include "gc.h"
 
-void allocateMemory(char** newFig, int rows, size_t cols){
-  memoryAlloc((void**)&newFig, sizeof(char*)*(rows + 1));
+void allocateMemory(char*** newFig, int rows, size_t cols){
+  memoryAlloc((void**)newFig, sizeof(char*)*(rows + 1));
   for(int i = 0; i < rows; i++)
-    memoryAlloc((void**)&newFig[i], sizeof(char)*(cols + 1));
+    memoryAlloc((void**)(*newFig + i), sizeof(char)*(cols + 1));
 }
 
-void unlinkMemory(char** fig){
+void unlinkMemory(char*** fig){
   countMemoryEntries();
-  for(int i = 0; fig[i]; i++)
-    unregisterPointer((void**)&fig[i]);
+  for(int i = 0; (*fig)[i]; i++)
+    unregisterPointer((void**)(*fig + i));
   countMemoryEntries();
-  unregisterPointer((void**)&fig);
+  unregisterPointer((void**)fig);
   countMemoryEntries();
 }
 
@@ -26,7 +26,7 @@ char** reverse(char** fig){
   while(fig[0][++cols]);
 
   char** newFig;
-  allocateMemory(newFig, rows, cols);
+  allocateMemory(&newFig, rows, cols);
 
   for(int i = 0; fig[i]; i++){
     for(int j = 0; fig[0][j]; j++)
@@ -34,7 +34,7 @@ char** reverse(char** fig){
     newFig[i][cols] = 0;
   }
   newFig[rows] = 0;
-  unlinkMemory(newFig);
+  unlinkMemory(&newFig);
   return newFig;
 }
 
